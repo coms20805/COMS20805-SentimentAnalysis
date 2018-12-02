572552d266ea284f74bd2eb8635b193e8e00ab16
NavyaZaveri <navyajayesh+1@gmail.com>
2018-12-02 20:16:11 +0000

    started writing documentation

--- backend/esclient/src/main/java/com/uob/esclient/example/App.java
+++ backend/esclient/src/main/java/com/uob/esclient/example/App.java
@@ -1,13 +1,39 @@
 package com.uob.esclient.example;
 
 import com.uob.esclient.client.ElasticClient;
-import com.uob.esclient.post.Post;
 import com.uob.esclient.search.SearchQuery;
 import com.uob.esclient.search.Strategy;
 
+import java.util.Date;
 import java.util.List;
 
+import lombok.Value;
+
+/*
+A simple demo app illustrating how to use ElasticClient.
+
+NOte: To run this app, you need to download the elasticsearch
+binaries from https://www.elastic.co/downloads/elasticsearch.
+
+ After downloading, execute the binaries: `bin/elasticsearch`
+ This starts up the low-level elastic search client needed
+ for our ElasticClient to work.
+ */
+
 public class App {
+
+
+    /* A generic Post that represents the documents
+    stored in the elasticsearch database
+    */
+    @Value
+    private static class MyPost {
+        private final String content;
+        private final Date timestamp;
+        private final String url;
+        private final double score;
+
+    }
 
     public static void main(String[] args) {
         ElasticClient client = new ElasticClient();
@@ -15,10 +41,11 @@ public class App {
             sq.limit = 100;
             sq.fieldToCompareAgainst = "content";
             sq.strategy = Strategy.FUZZY_MATCH;
-            sq.literalQuery = "scala";
+            sq.literalQuery = "pythn";
         }).build();
 
-        List<Post> posts = client.findPosts(searchQuery, Post.class);
+
+        List<MyPost> posts = client.findPosts(searchQuery, MyPost.class);
         posts.forEach(System.out::println);
     }
 }
--- backend/esclient/src/main/java/com/uob/esclient/search/GreedyMatcher.java
+++ backend/esclient/src/main/java/com/uob/esclient/search/GreedyMatcher.java
@@ -3,11 +3,11 @@ package com.uob.esclient.search;
 import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.index.query.MatchAllQueryBuilder;
-import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
 
 
-//matches against ALL posts
+//This Matcher attempts to match a query against ALL posts in the elastic search database. Can be used to test/sanity-check
+//if the database is populated.
 public final class GreedyMatcher extends Matcher {
     public GreedyMatcher(TransportClient client) {
         super(client);
