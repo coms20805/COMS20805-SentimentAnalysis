d0e315c3a9f99bfa52c687effa3812cd2c28f548
NavyaZaveri <navyajayesh+1@gmail.com>
2019-04-11 09:28:23 +0100

    make classifier persistent

--- backend/scraper/spam_classifier/classifier.py
+++ backend/scraper/spam_classifier/classifier.py
@@ -5,11 +5,36 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.multiclass import OneVsRestClassifier
 from sklearn.svm import SVC
 from itertools import chain
+from enum import Enum
 
 TRAIN_SIZE = 4400
 MAXIMUM_SENTIMENT_SCORE = 0.2
 VECTORIZER_FILENAME = "vectorizer.pkl"
 CLASSIFIER_FILENAME = "classifier.pkl"
+
+
+class Result(Enum):
+    SPAM = 1
+    NOT_SPAM = 0
+
+
+class PreTrainedClassifier:
+    def __init__(self):
+        self.model = _load_model()
+        self.vectorizer = _load_vectorizer()
+
+    def classify(self, post, key, verbose=False):
+        if key not in post:
+            raise ValueError(
+                "%s doesn't exist in the post. It has to be a valid attribute of the post we want to run the "
+                "spam classifier against", key)
+        content_vec = self.vectorizer.transform([post[key]])
+        result = self.model.predict(content_vec)[0]
+        probs = self.model.predict_proba(content_vec).tolist()
+        predict_proba = max(chain(*probs))
+        if verbose:
+            print("LOG: " + post[key] + " is classified as <" + result.upper() + "> with prob =  " + str(predict_proba))
+        return Result.SPAM if result == "spam" else Result.NOT_SPAM
 
 
 def _load_model():
@@ -36,7 +61,7 @@ def _save_model(spam_model):
         pickle.dump(spam_model, fd)
 
 
-def _save_vectroizer(vectorizer):
+def _save_vectorizer(vectorizer):
     with open(VECTORIZER_FILENAME, 'wb') as fd:
         pickle.dump(vectorizer, fd)
 
@@ -50,23 +75,12 @@ def train():
     vectorize_text = vectorizer.transform(train_data.v2)
     classifier.fit(vectorize_text, train_data.v1)
     _save_model(classifier)
-    _save_vectroizer(vectorizer)
-
-
-def is_spam(post, key, verbose=False):
-    model = _load_model()
-    vectorizer = _load_vectorizer()
-    content_vec = vectorizer.transform([post[key]])
-    result = model.predict(content_vec)[0]
-    probs = model.predict_proba(content_vec).tolist()
-    predict_proba = max(chain(*probs))
-    if verbose:
-        print("LOG: " + post[key] + " is classified as <" + result.upper() + "> with prob =  " + str(predict_proba))
-    return result == "spam"
+    _save_vectorizer(vectorizer)
 
 
 def main():
-    is_spam({"content": "udemy coupon for only 3 dollars", "score": 0}, "content", verbose=True)
+    clf = PreTrainedClassifier()
+    res = clf.classify({"content": "udemy coupon for only 3 dollars", "score": 0}, "content", verbose=True)
 
 
 if __name__ == '__main__':
