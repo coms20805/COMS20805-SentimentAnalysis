14ae422d071abfb0c60169b36940c9c6720cdbca
BenJamesPrice <benjprice99@gmail.com>
2019-04-13 20:24:25 +0100

    Add Spring tests

--- backend/rest_api/SentimentAnalysis/src/test/java/com/sentimentanalysis/SentimentAnalysis/SentimentAnalysisApplicationTests.java
+++ backend/rest_api/SentimentAnalysis/src/test/java/com/sentimentanalysis/SentimentAnalysis/SentimentAnalysisApplicationTests.java
@@ -2,15 +2,57 @@ package com.sentimentanalysis.SentimentAnalysis;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SentimentAnalysisApplicationTests {
+
+	@Autowired
+	private QueryController queryController;
+	@Autowired
+	private MockMvc mockMvc;
 
 	@Test
 	public void contextLoads() {
+		assertThat(queryController).isNotNull();
+		assertThat(mockMvc).isNotNull();
+	}
+
+	@Test
+	public void returnJSON() throws Exception {
+		this.mockMvc.perform(get("/api/search?query=a")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8));
+	}
+
+	@Test
+	public void returnValidListOfPosts() throws Exception{
+		this.mockMvc.perform(get("/api/search?query=java")).andDo(print())
+				.andExpect(status().isOk())
+				.andExpect(jsonPath("$.rating", is(notNullValue())))
+				.andExpect(jsonPath("$.posts").isArray())
+				.andExpect(jsonPath("$.posts[0].*", hasSize(4)))
+				.andExpect(jsonPath("$.posts[0].content", not(isEmptyOrNullString())))
+				.andExpect(jsonPath("$.posts[0].timestamp", not(isEmptyOrNullString())))
+				.andExpect(jsonPath("$.posts[0].url", not(isEmptyOrNullString())))
+				.andExpect(jsonPath("$.posts[0].score", is(notNullValue())))
+				.andExpect(jsonPath("$.posts[0].score", greaterThanOrEqualTo(-1.0)))
+				.andExpect(jsonPath("$.posts[0].score", lessThanOrEqualTo(1.0)));
 	}
 
 }
