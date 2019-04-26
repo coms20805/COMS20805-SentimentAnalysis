b2ae582bd1ab6f3e795a901b6797b27cfe4802cd
mnk179 <martynas.nor@gmail.com>
2019-04-26 09:05:31 +0100

    Integrate suggestions code

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -2,23 +2,59 @@ import React, { Component } from "react";
 import "../styles/searchBar.css";
 import {Button, FormControl, FormGroup, Glyphicon, InputGroup} from "react-bootstrap";
 
+export default class SearchBar extends Component {
+    availableQueries = [
+        "Java",
+        "Python",
+        "JavaScript",
+        "JSON",
+        "MATLAB",
+        "JQuery",
+        "CSharp",
+        "Opal",
+        "Ruby",
+        "SQL",
+        "Swift",
+      ];
 
-
-class SearchBar extends Component {
     state = {
-        value: ""
+        value: "",
+        suggestions: []
     };
-
+    
     componentDidMount() {
-        this.setState({ value: this.props.value });
+        this.setState({value: this.props.value});
     }
 
     handleChange(e) {
-        this.setState({ value: e.target.value });
+        let value = e.target.value;
+        let suggestions = [];
+        if (value.length > 0) {
+            const regex = new RegExp(`^${value}`, 'i');
+            suggestions = this.availableQueries.sort().filter(v => regex.test(v));
+        }
+
+        this.setState({suggestions, value});
     }
 
     componentWillReceiveProps(props) {
         this.setState({value: props.value});
+    }
+
+    suggestionSelected(value){
+        this.setState({value, suggestions: []});
+    }
+
+    renderSuggestions() {
+        const suggestions = this.state.suggestions;
+        if (suggestions.length === 0) {
+          return null;
+        }
+        return (
+          <ul>
+              {suggestions.map((item) => <li onClick={() => this.suggestionSelected(item)}>{item}</li>)}
+          </ul>
+        );
     }
 
     render() {
@@ -42,11 +78,10 @@ class SearchBar extends Component {
                                 </Button>
                             </InputGroup.Button>
                         </InputGroup>
+                        {this.renderSuggestions()}
                     </FormGroup>
                 </form>
             </div>
         );
     }
 }
-
-export default SearchBar;
