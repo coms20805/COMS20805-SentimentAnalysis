43ea5b386a0051020a2d0e30a664ce7f91aaeb0e
mnk179 <martynas.nor@gmail.com>
2019-04-27 09:38:22 +0100

    Add badges to posts

--- frontend/src/components/Post/PostBox.jsx
+++ frontend/src/components/Post/PostBox.jsx
@@ -2,10 +2,37 @@ import React, { Component } from "react";
 import Moment from "react-moment";
 
 class PostBox extends Component {
+
+    state = {
+        ratingNumber: 0,
+        badgeClasses: "badge badge-light"
+    }
+
+    parseRating() {
+        const ratingNumber = parseFloat(Math.round(this.props.post.score * 100) / 100).toFixed(2);
+
+        let badgeClasses = "badge badge-";
+        if (ratingNumber < 0) {
+            badgeClasses += "danger";
+        }
+        else if (ratingNumber > 0) {
+            badgeClasses += "success";
+        }
+        else {
+            badgeClasses += "light";
+        }
+
+        this.setState({ratingNumber, badgeClasses});
+    }
+
+    componentDidMount() {
+        this.parseRating();
+    }
+
     render() {
         return(
             <tr className="post-box">
-                <td data-label="Score">{parseFloat(Math.round(this.props.post.score * 100) / 100).toFixed(2)}</td>
+                <td data-label="Score"><span className={this.state.badgeClasses}>{this.state.ratingNumber}</span></td>
                 <td data-label="Date"><Moment format="D MMM YYYY">{this.props.post.timestamp}</Moment></td>
                 <td data-label="Content">{this.props.post.content}</td>
                 <td data-label="URL"><a href={this.props.post.url} target="_blank" rel="noopener noreferrer">See post</a></td>
