1b1dbb68b30339b41a772cbfe4559a39977ac2c7
mnk179 <martynas.nor@gmail.com>
2019-04-27 09:31:11 +0100

    Integrate coloured badges for RatingBox

--- frontend/src/components/RatingBox.jsx
+++ frontend/src/components/RatingBox.jsx
@@ -1,24 +1,54 @@
 import React, { Component } from "react";
 
 class RatingBox extends Component {
-    render() {
+
+    state = {
+        ratingLiteral: "neutral",
+        ratingNumber: 0,
+        ratingClasses: "rating badge badge-light"
+    }
+
+    getRoundedRating() {
         const ratingNumber = parseFloat(Math.round(this.props.rating * 100) / 100).toFixed(2);
+        
+        return ratingNumber;
+    }
+
+    parseRating() {
+        const ratingNumber = this.getRoundedRating();
+
         let ratingLiteral = "neutral";
+        let ratingClasses = "rating badge badge-";
         if (ratingNumber < 0) {
             ratingLiteral = "negative";
+            ratingClasses += "danger";
         }
         else if (ratingNumber > 0) {
             ratingLiteral = "positive";
+            ratingClasses += "success";
+        }
+        else {
+            ratingClasses += "light";
         }
         if (Math.abs(ratingNumber) > 0.5) {
             ratingLiteral = "very " + ratingLiteral;
         }
+
+        this.setState({ratingLiteral, ratingNumber, ratingClasses})
+    }
+
+    componentDidMount() {
+        this.parseRating();
+    }
+
+    render() {
+        
         return(
             <div className="rating-box">
-                <div className="rating-description"><span>The sentiment towards this piece of tech is <strong>{ratingLiteral}</strong></span></div>
-                <div className="rating">
+                <div className="rating-description"><span>The sentiment towards this piece of tech is <strong>{this.state.ratingLiteral}</strong></span></div>
+                <div className={this.state.ratingClasses}>
                     <div className="rating-text">Rating</div>
-                    <div className="rating-number">{ratingNumber}</div>
+                    <div className="rating-number">{this.state.ratingNumber}</div>
                 </div>
             </div>
         );
